b46add431fdf05c473e0f42ecda244df1a88f64a
Youssif Mostafa <youssifmostafa798@gmail.com>
2026-08-06 19:15:04 +0300

    Update Supabase publishable key in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ Future<void> main() async {
 
   await Supabase.initialize(
     url: 'https://tonctmdcntftugdskqmb.supabase.co',
-    publishableKey: 'sb_publishable_KyM603zhx0TqZZQgHjecmw_YUxXB-Ee ',
+    publishableKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvbmN0bWRjbnRmdHVnZHNrcW1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2ODYyNTIsImV4cCI6MjEwMTI2MjI1Mn0.Gsuo7YaYZWAx5bB7MMU16qEPduU-vSWV4FyR07SB5Yc ',
   );
 
   runApp(const ProviderScope(child: MyApp()));
